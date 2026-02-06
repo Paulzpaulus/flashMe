07df6a0e513a7104ad44263f95e0dfd2141198b1
Paulspaulus <pauls.schweppe@gmail.com>
2026-02-06 09:18:28 +0100

    refactoring file structure

--- app/main.py
+++ app/main.py
@@ -1,40 +1,7 @@
-from fastapi import FastAPI, HTTPException
-from models.user_model import User
-
-
+from fastapi import FastAPI
+from routers.user_routes import router
 
 app = FastAPI()
+app.include_router(router)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello You"}
-
-# read ALL users
-@app.get("/users")
-async def show_users(User: User):
-    if not User:
-        raise HTTPException(status_code=404, detail="No Users found")
-    return User
-
-
-#read ONE user
-@app.get("/users/{user_id})")
-async def show_a_user(user_id: int):
-    return {"user_id": user_id}
-
-#create user
-@app.get("/users")
-async def create_user():
-    pass
-
-
-#edit user
-@app.put("/users/{user_id}")
-async def edit_user(user_id: int):
-    pass
-
-#delete user
-@app.delete("/users/{user_id}")
-async def delete_user(user_id: int):
-    pass
---
+++ routers/user_routes.py
@@ -0,0 +1,39 @@
+
+from fastapi import HTTPException, APIRouter
+from models.user_model import User
+
+
+router = APIRouter()
+
+@router.get("/")
+async def root():
+    return {"message": "Hello You"}
+
+# read ALL users
+@router.get("/users")
+async def show_users(User: User):
+    if not User:
+        raise HTTPException(status_code=404, detail="No Users found")
+    return User
+
+
+#read ONE user
+@router.get("/users/{user_id}")
+async def show_a_user(user_id: int):
+    return {"user_id": user_id}
+
+#create user
+@router.get("/users")
+async def create_user():
+    pass
+
+
+#edit user
+@router.put("/users/{user_id}")
+async def edit_user(user_id: int):
+    pass
+
+#delete user
+@router.delete("/users/{user_id}")
+async def delete_user(user_id: int):
+    pass
